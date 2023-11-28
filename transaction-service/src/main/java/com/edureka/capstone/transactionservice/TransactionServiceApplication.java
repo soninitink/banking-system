cd275ae9f29262fe4a20f476fbff9f0a9aed8200
Nitin Soni <cool.nitin18@gmail.com>
2023-11-28 00:03:08 -0800

    CapStone project base created with required classes.

--- transaction-service/src/main/java/com/edureka/capstone/transactionservice/TransactionServiceApplication.java
+++ transaction-service/src/main/java/com/edureka/capstone/transactionservice/TransactionServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.transactionservice;
+package com.edureka.capstone.transactionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
